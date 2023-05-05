9f76e80b83d489564081b795a30c9f8a767fa3e7
Peter Bortchagovsky <github@Liksu.kiev.ua>
2023-05-05 21:40:05 +0200

    Remove middleware in favour redirects

--- README.md
+++ README.md
@@ -3,19 +3,19 @@
 Next.js application template with predefined libraries and configurations.
 
 ## Contains predefined libraries and configurations:
-- [x] [Next.js](https://nextjs.org/)
-- [x] [Mantine](https://mantine.dev/)
+- [x] [Next.js](https://nextjs.org/) is a React framework that provides infrastructure and simple development experience for server side rendered (SSR) and statically exported applications. Currently no `/app` folder support provided.
+- [x] [Mantine](https://mantine.dev/) - React components and hooks library with native TypeScript support and without external dependencies. In the template included:
   - core
   - dates
   - form
   - modals
   - notifications
-- [x] [next-i18next](https://next.i18next.com/)
-- [x] [NextAuth.js](https://next-auth.js.org/)
-- [x] [MongoDB](https://www.mongodb.com/)
-- [x] [MDX](https://mdxjs.com/)
-- [x] [PM2](https://pm2.keymetrics.io/)
-- [x] [React GA4](https://www.npmjs.com/package/react-ga4)
+- [x] [next-i18next](https://next.i18next.com/) - internationalization framework for Next.js.
+- [x] [NextAuth.js](https://next-auth.js.org/) - authentication library for Next.js applications.
+- [x] [MongoDB](https://www.mongodb.com/) - NoSQL database.
+- [x] [MDX](https://mdxjs.com/) - included support for MDX files.
+- [x] [PM2](https://pm2.keymetrics.io/) - process manager for Node.js applications.
+- [x] [React GA4](https://www.npmjs.com/package/react-ga4) - Google Analytics 4 for React.
 - [x] [TypeScript](https://www.typescriptlang.org/)
 - [x] [ESLint](https://eslint.org/)
 
--- next.config.js
+++ next.config.js
@@ -1,36 +1,51 @@
 const {i18n} = require("./next-i18next.config");
 
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
 })
 
+const authCookie = {
+    type: 'cookie',
+    key: 'next-auth.session-token',
+    value: '^[0-9a-f-]{36}$',
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
-  pageExtensions: ['ts', 'tsx', 'jsx', 'md', 'mdx', 'html'],
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      ...{
-        topLevelAwait: true
-      }
+    redirects() {
+      return [
+        {
+          source: '/profile/:path*',
+          missing: [authCookie],
+          permanent: false,
+          destination: '/sign-in',
+        },
+      ]
+    },
+    i18n,
+    pageExtensions: ['ts', 'tsx', 'jsx', 'md', 'mdx', 'html'],
+    webpack: (config) => {
+        config.experiments = {
+            ...config.experiments,
+            ...{
+                topLevelAwait: true
+            }
+        }
+        return config
+    },
+    serverRuntimeConfig: {
+        mongoURI: process.env.MONGO_URI,
+        mongoDB: process.env.MONGO_DB,
+        nextAuthURL: process.env.NEXTAUTH_URL,
+        nextAuthSecret: process.env.NEXTAUTH_SECRET,
+        googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
+        googleId: process.env.GOOGLE_ID,
+        googleSecret: process.env.GOOGLE_SECRET,
     }
-    return config
-  },
-  serverRuntimeConfig: {
-    mongoURI: process.env.MONGO_URI,
-    mongoDB: process.env.MONGO_DB,
-    nextAuthURL: process.env.NEXTAUTH_URL,
-    nextAuthSecret: process.env.NEXTAUTH_SECRET,
-    googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
-    googleId: process.env.GOOGLE_ID,
-    googleSecret: process.env.GOOGLE_SECRET,
-  }
 }
 
 module.exports = withMDX(nextConfig)
